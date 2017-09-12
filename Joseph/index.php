df15ba29eb2850aa7f5d564e8d946942e0f2e407
Joseph990618 <30561976+Joseph990618@users.noreply.github.com>
2017-09-12 10:55:15 +0800

    lol

--- Joseph/index.php
+++ Joseph/index.php
@@ -2,9 +2,31 @@
 <html lang="en">
 <head>
 	<meta charset="UTF-8">
-	<title>Document</title>
+	<title>My statement</title>
 </head>
 <body>
-	<p>hello is me</p>
+	<?php 
+
+	$myName = 'Joseph';
+
+	$myGender = 'male';
+
+	$myNickname = '左手摇';
+
+	$myBirthday = 'June 18';
+
+	$myStatus = 'still single';
+
+	$selfIntroduction1 = 'Hi there!! My name is ' . $myName . ', but sometimes my friends would like to call me ' . $myNickname . '. I am a male.';
+
+	$selfIntroduction2 = "My birthday is on $myBirthday. I am $myStatus right now.";
+
+	echo $selfIntroduction1;
+
+	echo '<br>';
+
+	echo $selfIntroduction2;
+
+	?>
 </body>
 </html>
