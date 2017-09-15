283e00a495ab05de344a3a25dc9ec69821fa1b60
ywpin <wakaka160216@gmail.com>
2017-09-15 23:11:24 +0800

    done

---
+++ weipin/taskc.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="UTF-8">
+		<title>Task</title>
+	</head>
+	<body>
+		<?php 
+
+			$myName = 'Yap Wei Pin, ';
+
+			$greeting = 'can call me ';
+
+			$nickname = 'Pin';
+
+			$greeting1 = ' my birthday is ';
+
+			$myBirthday = '10-8. ';
+
+			$customMessage = 'Nothing left~';
+
+			echo 'Hello, my name is ' . $myName . $greeting . $nickname . $greeting1 . $myBirthday . $customMessage;
+
+			echo '<br>';
+
+			echo "Hello, my name is $myName $greeting $nickname $greeting1 $myBirthday $customMessage";
+
+		 ?>
+	</body>
+</html>
