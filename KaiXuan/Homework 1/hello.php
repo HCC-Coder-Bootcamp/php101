3271d472cad672d5bbd4aca929ae6477085b823b
KaiXuan2243 <chen123456626@gmail.com>
2017-09-15 22:26:08 +0800

    upload

---
+++ KaiXuan/Homework 1/hello.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Document</title>
+</head>
+<body>
+	<?php
+	$name = "Tan Kai Xuan";
+	$nickName = "Kai Xuan";
+	$Gender = "male";
+	$birthday = "20 August";
+	$school = "Nonstop IT Academy";
+	$favourite = "collect pictures";
+
+	echo 'Hello everyone, myname is ' . $name . '.' . 'My nick name is ' . $nickName . '.' . 'My gender is ' . $Gender . '.' . 'My birthday is on ' . $birthday . '.' . 'Now my school is ' . $school . '.' . 'My favourite is ' . $favourite '.'
+	echo "<br>";
+	echo "hello everyone, my name is $name, my nick name is $nickname and gender is $gender. My birthday is on $birthday, now my school is $School. My favourite is $favourite.";
+?> 
+</body>
+</html>
