f6b37c2cbd9d5cfe037ac86c0c394d181d096d4a
Phoebe <phoebe@powerstation.sg>
2017-09-17 01:34:36 +0800

    my work done

---
+++ chloe/hello.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>hello world</title>
+</head>
+<body>
+	
+	<h1>hello world</h1>
+
+	<?php 
+	// echo 'Hello PHP'; 
+		/**
+		 * 
+		 */
+	$myName = 'Chloe';
+
+	$myGender = 'Female';
+
+	$selfintroduction = "Hi, I' m  $myName. \t I am $myGender" ;
+	//$greeting = 'Welcone home, ' . $myName . '!';
+	
+	$greeting = "Welcone home, $myName!";
+
+	echo $selfintroduction ;
+	?>
+</body>
+</html>   
---
+++ chloe/homework.php
@@ -0,0 +1,29 @@
+<?php 
+
+	$myName = 'Chloe';
+
+	$nickname = 'Chlo';
+
+	$birthday = '1995/03/31';
+
+	$custommessage = 'shh...that\'s my details.';
+
+	echo 'Hi my name is ' . $myName . ', you can call me ' . $nickname . '. My birthday is on ' . $birthday . '. Oops ' . $custommessage ;
+
+ ?>
+
+<br>
+
+ <?php 
+
+ 	$myName = "Chloe";
+
+ 	$nickname = "Chlo";
+
+ 	$birthday = "1995/03/31";
+
+ 	$custommessage = "shh...that's my details.";
+
+ 	echo "Hi my name is $myName, you can call me $nickname. My birthday is on $birthday. Oops $custommessage";
+
+  ?>
