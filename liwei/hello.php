8185fc52b9d5670754b2f55cd81b89650b535fe6
tlweiii6465 <tlweiii646564@gmail.com>
2017-09-17 09:37:41 +0800

    update

--- liwei/hello.php
+++ liwei/hello.php
@@ -1,46 +1,46 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<title>document</title>
+    <title>document</title>
 </head>
 <body>
-	<h1>hello word</h1>
-	<?php
-	 		$myName = 'Tlwei';
+    <h1>hello word</h1>
+    <?php
+       $myName = 'Tlwei';
 
-	 		$greeting = 'Welcome home, ' . $myName . '!';
+       $greeting = 'Welcome home, ' . $myName . '!';
 
-	 		echo $greeting;
-	 ?>
-	 <br>
-	 <?php
-	 		
-	 		$myName = 'Tlwei';
+       echo $greeting;
+     ?>
+     <br>
+     <?php
+             
+       $myName = 'Tlwei';
 
-	 		$myGender = 'Boy';
+       $myGender = 'Boy';    
 
-	 		$selfintroduction = 'Hi, im ' . $myName . ', im a ' . $myGender . ' haha ';
+       $selfintroduction = 'Hi, im,' . $myName . ' im a ' . $myGender . ' haha ';
       //double quote 
-	 		//$selfintroduction = "Hi, im  $myName  im a  $myGender  haha "; 
+             //$selfintroduction = "Hi, im $myName im a $myGender haha "; 
 
-	 		echo $selfintroduction;
-	 ?>
-	 <br>
-	 <?php
-	 		
-	 		$myName = 'Tlwei';
+       echo $selfintroduction;
+     ?>
+     <br>
+     <?php
+             
+       $myName = 'Tlwei';
 
-	 		$myNickname = 'Liwei';
+       $myNickname = 'Liwei';
 
-	 		$myBirthday = '11/18';
+       $myBirthday = '11/18';
 
-	 		$myCustomemassage = ' haha haha haha';
+       $myCustomemassage = ' haha haha haha';
 
-	 		$greetingline = 'Hi, im ' . $myName . ', my nickname is ' . $myNickname . ' my Birthday is ' . $myBirthday . $myCustomemassage;
+       $greetingline = 'Hi, im, ' . $myName . ' my nickname is ' . $myNickname . ' my Birthday is ' . $myBirthday . $myCustomemassage;
       //double quote
       //$greetingline = "Hi, im $myName, my nickname is $myNickname my Birthday is $myBirthday $myCustomemassage";
 
-	 		echo $greetingline;
-	 ?>
+       echo $greetingline;
+     ?>
 </body>
 </html>
